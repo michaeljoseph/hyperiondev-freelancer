750658b5bbfd9d21a50fe048ad68588a49abab97
Michael Joseph <michaeljoseph+github@gmail.com>
2022-12-13 09:29:01 +0200

    Remove unused code

--- section-c/isbn.py
+++ section-c/isbn.py
@@ -60,39 +60,6 @@ ISBN_10_MULTIPLIER = lambda index: 10 - index
 ISBN_13_MULTIPLIER = lambda index: 1 if index % 2 == 0 else 3
 
 
-def is_valid_isbn10(candidate: str, multiplier = ISBN_10_MULTIPLIER) -> bool:
-    # initialise digit sum counter
-    total = 0
-
-    # loop through the isbn digits
-    for index, digit in enumerate(candidate):
-        # handle trailing X digit
-        if digit == 'X':
-            if index != 9:
-                return False
-
-            digit = '10'
-
-        # digit is a string, so convert to a number before multplying
-        total += int(digit) * multiplier(index)
-
-    # valid if cleanly divisible by 10
-    return total % 11 == 0
-
-
-
-def is_valid_isbn13(candidate: str, multiplier = ISBN_13_MULTIPLIER) -> bool:
-    # initialise digit sum counter
-    total = 0
-
-    # loop through the isbn digits
-    for index, digit in enumerate(candidate):
-        # digit is a string, so convert to a number before multplying
-        total += int(digit) * multiplier(index)
-
-    # valid if cleanly divisible by 10
-    return total % 10 == 0
-
 class InvalidIsbn(Exception):
     pass
 
